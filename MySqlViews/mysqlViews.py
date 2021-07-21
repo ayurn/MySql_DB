baba94ab448ffaedafbaba2f1a4d78101f7285ba
ayurn <ninaweayur@gmail.com>
2021-07-21 17:50:06 +0530

    [Ayur] Add : creating mysql view from employee table.

---
+++ MySqlViews/mysqlViews.py
@@ -0,0 +1,38 @@
+'''
+@Author: Ayur Ninawe
+@Date: 2021-07-21 10:00:30
+@Last Modified by: Ayur Ninawe
+@Last Modified time: 2021-07-21 10:00:30
+@Title : Connecting database with python and performing view operations on tables.
+'''
+import mysql.connector
+from mysql.connector import Error
+from decouple import config
+import Log 
+
+database_con = mysql.connector.connect(host= config('HOST'),
+                                        database=config('DATABASE'),
+                                        user=config('DBUSER'),
+                                        password=config('PASSWORD'))
+
+class Database:
+    
+    def createView():
+        """
+        Description:
+            function will create mysql view from given columns from table.
+        """
+        try:
+            cursor = database_con.cursor()
+            createView_query ="create view employee_info as select FIRST_NAME, INCOME from EMPLOYEE"
+            cursor.execute(createView_query)
+            Log.logging.debug('View created')
+        except Exception as e:
+            Log.logging.error(e)
+            
+    
+            
+if __name__ == '__main__':
+    viewObj = Database
+    viewObj.createView()
+    
